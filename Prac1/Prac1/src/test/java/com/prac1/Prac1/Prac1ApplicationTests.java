2189296fdc849a9ba73880fb7c90260f5b9a2163
ShubhamUpad <01shubhamupadhyay@gmail.com>
2024-11-17 20:24:15 +0530

    Added prac1

---
+++ Prac1/Prac1/src/test/java/com/prac1/Prac1/Prac1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prac1.Prac1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Prac1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
