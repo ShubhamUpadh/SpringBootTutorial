75d179f558e6bd2d88966afcbec2760b587b9ca0
ShubhamUpad <01shubhamupadhyay@gmail.com>
2024-11-20 01:26:13 +0530

    Finished lecture #9, spring.xml

---
+++ Prac2/src/main/java/org/Prac2/Dev.java
@@ -0,0 +1,10 @@
+package org.Prac2;
+
+public class Dev {
+    public Dev(){
+        System.out.println("Constructing");
+    }
+    public void build(){
+        System.out.println("Working on this project");
+    }
+}
---
+++ Prac2/src/main/java/org/Prac2/Laptop.java
@@ -0,0 +1,10 @@
+package org.Prac2;
+
+public class Laptop {
+    public Laptop(){
+        System.out.println("Laptop created");
+    }
+    public void compile(){
+        System.out.println("Compiling");
+    }
+}
