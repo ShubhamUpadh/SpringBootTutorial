cf65367047a72cce924f64b7cf1639f798edca88
ShubhamUpad <01shubhamupadhyay@gmail.com>
2024-11-24 15:43:33 +0530

    Lecture15, @RequestBody, @RequestMapping, @PathVariable

--- Prac3/simpleWebApp/src/main/java/com/Prac3/simpleWebApp/controller/ProductController.java
+++ Prac3/simpleWebApp/src/main/java/com/Prac3/simpleWebApp/controller/ProductController.java
@@ -3,8 +3,7 @@ package com.Prac3.simpleWebApp.controller;
 import com.Prac3.simpleWebApp.model.Product;
 import com.Prac3.simpleWebApp.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -13,8 +12,18 @@ public class ProductController {
     @Autowired
     ProductService productService;
 
-    @RequestMapping("/products")
+    @GetMapping("/products")
     public List<Product> getProducts(){
         return productService.getProducts();
+    }
+
+    @GetMapping("/products/{id}")
+    public Product getProductByID(@PathVariable int id){
+        return productService.getProductById(id);
+    }
+
+    @PostMapping("/products")
+    public Product addProduct(@RequestBody Product prod){
+        return productService.addProduct(prod);
     }
 }
--- Prac3/simpleWebApp/src/main/java/com/Prac3/simpleWebApp/service/ProductService.java
+++ Prac3/simpleWebApp/src/main/java/com/Prac3/simpleWebApp/service/ProductService.java
@@ -3,18 +3,33 @@ package com.Prac3.simpleWebApp.service;
 import com.Prac3.simpleWebApp.model.Product;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 @Service
 public class ProductService {
 
-    List<Product> products = Arrays.asList(
+    List<Product> products = new ArrayList<>(Arrays.asList(
             new Product(101,"IPhone",5000),
             new Product(102,"Samsung",1500),
-            new Product(103, "Mi",4000));
+            new Product(103, "Mi",4000)));
 
     public List<Product> getProducts(){
         return products;
+    }
+
+    public Product getProductById(int id){
+        for (Product product : products){
+            if (product.getProdID() == id){
+                return product;
+            }
+        }
+        return null;
+    }
+
+    public Product addProduct(Product prod) {
+        products.add(prod);
+        return prod;
     }
 }
