905768e633e15e43e19431e10d3a7dc1155f889b
ShubhamUpad <01shubhamupadhyay@gmail.com>
2024-11-25 16:23:49 +0530

    added Prac4

---
+++ Prac4/ecom-proj/src/test/java/com/Prac4/ecom_proj/EcomProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Prac4.ecom_proj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomProjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
